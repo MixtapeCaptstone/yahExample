0c2a64d0b2ba15898d91af65c9fceb0688eebc92
Do Nguyen <doclimbs@gmail.com>
2015-10-27 15:14:47 -0700

    Cleaning up drag and drop functions. Adding database and deletion functionality. Also adding user template.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
+Song = new Mongo.Collection('song');
+
+// Meteor.publish('song', function() {
+//   return Song.find({});
+// });
+
 Meteor.methods( {
   checkYT : function (search) {
-    // search = 'macklemore';
     var url = "https://www.googleapis.com/youtube/v3/search";
     var params = {
         key: apiKey,
@@ -11,5 +16,15 @@ Meteor.methods( {
     };
     var res = Meteor.http.call('GET', url, {params: params});
     return res;
-}
+  },
+  addSong: function (text) {
+    Song.insert({
+      text: text.text,
+      createdAt: new Date(),
+      id: text.id,
+      pic: text.pic
+      // owner: Meteor.userId(),
+      // username: Meteor.user().username
+    });
+  }
 });
